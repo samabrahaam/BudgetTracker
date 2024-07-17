8086c3ab9cec77978243465d80c870e676861ab8
samabrahaam <samcabraham9@gmail.com>
2024-07-17 08:27:49 -0500

    update allowed host

--- budget_app/budget_app/settings.py
+++ budget_app/budget_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!4iwc1*!4st((hg5j^m_wt=!^o0@mbrfhb!e%3#c=w8v)#d9s9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0:8000', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
